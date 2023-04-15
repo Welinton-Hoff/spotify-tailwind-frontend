3c69225e33be65721f85cd445742b12d0e6899f6
welinton <welinton.h.r@hotmail.com>
2023-04-15 11:32:37 +0100

    Init spotify web clone

---
+++ src/components/Footer.tsx
@@ -0,0 +1,63 @@
+import {
+  Play,
+  Mic2,
+  Repeat,
+  Volume,
+  Shuffle,
+  Laptop2,
+  SkipBack,
+  Maximize2,
+  LayoutList,
+  SkipForward,
+} from "lucide-react";
+import Image from "next/image";
+
+export function Footer() {
+  return (
+    <footer className="bg-zinc-900 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image
+          width={56}
+          height={56}
+          alt="JEND COLLECTION"
+          src="/jend-collection.jpeg"
+        />
+        <div className="flex flex-col">
+          <strong className="font-normal">JEND COLLECTION</strong>
+          <span className="text-xs text-zinc-400">JEND</span>
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="flex items-center gap-6">
+          <Shuffle size={20} className="text-zinc-200" />
+          <SkipBack size={20} className="text-zinc-200" />
+          <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+            <Play />
+          </button>
+          <SkipForward size={20} className="text-zinc-200" />
+          <Repeat size={20} className="text-zinc-200" />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <span className="text-xs text-zinc-400">0:58</span>
+          <div className="h-1 rounded-full w-96 bg-zinc-600">
+            <div className="bg-zinc-200 w-40 h-1 rounded-full" />
+          </div>
+          <span className="text-xs text-zinc-400">2:14</span>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+        <Mic2 size={20} />
+        <LayoutList size={20} />
+        <Laptop2 size={20} />
+        <div className="flex items-center gap-2">
+          <Volume size={20} />
+          <div className="h-1 rounded-full w-24 bg-zinc-600">
+            <div className="bg-zinc-200 w-10 h-1 rounded-full" />
+          </div>
+        </div>
+        <Maximize2 size={20} />
+      </div>
+    </footer>
+  );
+}
